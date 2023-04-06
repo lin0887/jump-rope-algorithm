78dc480371e59f5d0030a424a3739636d655d8ce
lin <ww70611@gmail.com>
2023-04-06 09:38:19 +0800

    docs:建立 requirements.txt 並更新 README

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import post
 import counter
+import os
 
-input_path = '..\\input\\A1102.MOV'
-output_path = '..\\output\\'+input_path.split('\\')[-1]
-student = post.Post_dection(input_path)
-post_df = student.dection()
+if os.path.isdir('..\\output\\') != True:
+    os.system("mkdir {}".format('..\\output\\'))
+    print("create folder: {}".format('..\\output\\'))
 
-a = counter.Body_point(post_df)
-for i in range(2):
-    a.get_amplitude()
-    a.get_wavelength()
+files = os.listdir('..\\input\\')
+
+for file in files:
     
-ans = a.jump_rope_count()  
-print(ans)
-f = a.make_flag()
+    input_path = '..\\input\\'+file
+    output_path = '..\\output\\'+file
+    
+    student = post.Post_dection(input_path)
+    post_df = student.dection()
 
-student.make_output_vidoe(output_path,f)
+    a = counter.Body_point(post_df)
+    for i in range(2):
+        a.get_amplitude()
+        a.get_wavelength()
+        
+    ans = a.jump_rope_count()  
+    print('\ntime :{}\n'.format(ans))
+    f = a.make_flag()
+
+    student.make_output_vidoe(output_path,f)
 
